3a20fa2287969504a55658ce1adecbf552deece8
vivax <vivax3794@protonmail.com>
2025-04-20 00:37:54 +0200

    test: ensure sibling text nodes arent collapsed

--- natrix/tests/actual_tests/simple_reactivty.rs
+++ natrix/tests/actual_tests/simple_reactivty.rs
@@ -18,6 +18,7 @@ impl Component for Counter {
     fn render() -> impl Element<Self> {
         e::button()
             .id(BUTTON_ID)
+            .text("value: ")
             .child(|ctx: R<Self>| *ctx.value)
             .on::<events::Click>(|ctx: E<Self>, _| *ctx.value += 1)
     }
@@ -28,7 +29,7 @@ fn renders_initial() {
     crate::mount_test(Counter { value: 0 });
 
     let button = crate::get(BUTTON_ID);
-    assert_eq!(button.text_content(), Some("0".to_owned()));
+    assert_eq!(button.text_content(), Some("value: 0".to_owned()));
 }
 
 #[wasm_bindgen_test]
@@ -36,7 +37,7 @@ fn uses_initial_data() {
     crate::mount_test(Counter { value: 123 });
 
     let button = crate::get(BUTTON_ID);
-    assert_eq!(button.text_content(), Some("123".to_owned()));
+    assert_eq!(button.text_content(), Some("value: 123".to_owned()));
 }
 
 #[wasm_bindgen_test]
@@ -46,11 +47,11 @@ fn updates_text() {
     let button = crate::get(BUTTON_ID);
 
     button.click();
-    assert_eq!(button.text_content(), Some("1".to_owned()));
+    assert_eq!(button.text_content(), Some("value: 1".to_owned()));
 
     button.click();
-    assert_eq!(button.text_content(), Some("2".to_owned()));
+    assert_eq!(button.text_content(), Some("value: 2".to_owned()));
 
     button.click();
-    assert_eq!(button.text_content(), Some("3".to_owned()));
+    assert_eq!(button.text_content(), Some("value: 3".to_owned()));
 }
